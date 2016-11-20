0ec8ae6b728d41577e2df760ea24748d17be3ac6
buiquangthang <buiquangthangit@gmail.com>
2016-11-20 11:34:11 +0700

    Initialize

---
+++ ProxyMVCv1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProxyMVCv1.MvcApplication" Language="C#" %>
---
+++ ProxyMVCv1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProxyMVCv1.Startup))]
+namespace ProxyMVCv1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
